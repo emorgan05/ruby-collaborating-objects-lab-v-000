43f5a3179001d4dda58d2fa5c60b433daf6f7622
Emily Morgan <emorgan05@gmail.com>
2017-09-30 16:06:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
   def self.new_by_filename(file)
     array = file.split(/\s\-\s/)
     song = self.new(array[1])
-    song.artist = array[0]
+    song.artist_name = array[0]
     song
   end
 
