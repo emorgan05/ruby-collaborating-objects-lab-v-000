b720905c90d039b73105b8f7b3dc1f9b5de455e7
Emily Morgan <emorgan05@gmail.com>
2017-09-30 15:17:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_accessor :name
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
@@ -14,5 +14,12 @@ class Artist
 
   def save
     @@all << self
+  end
+
+  def self.find_or_create_by_name
+  end
+
+  def print_songs
+    puts self.songs
   end
 end
